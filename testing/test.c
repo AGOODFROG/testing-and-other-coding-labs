109d8e40af44cdcf0750d568b97ca7982ee66142
AGOODFROG <caden.j.quirion@gmail.com>
2025-12-29 18:06:00 -0500

    added some nice text and a while loop

--- testing/test.c
+++ testing/test.c
@@ -3,32 +3,31 @@
 int main(void){   
     // thing
     /* other thing*/
-    int a;
-    char op;
-    int b;
-    int c;
-    // I think code like this is why rust in stuff are a thing
-    printf("type in a number:\n");
-    scanf("%d" "%c" "%d", &a, &op, &b);
-    printf("%d" "%c" "%d" , a, op, b);
-    printf("\n");
+    while(1){
+        int a;
+        char op;
+        int b;
+        int c;
+        // I think code like this is why rust in stuff are a thing
+        printf("type in a number(ctrl-c to exit):\n");
+        scanf("%d" "%c" "%d", &a, &op, &b);
+        printf("%d" "%c" "%d" , a, op, b);
+        printf("\n");
 
-    if(op == '+'){
-        c = a+b;
-    }else if(op == '-'){
-        c=a-b;
-    }else if (op == '*') {
-        c=a*b;
-    }else if (op == '/') {
-        c= a/b;
-    }else{
-        printf("opporator/whitespace not suported \n");
-        c = -1;
+        if(op == '+'){
+            c = a+b;
+        }else if(op == '-'){
+            c=a-b;
+        }else if (op == '*') {
+            c=a*b;
+        }else if (op == '/') {
+            c= a/b;
+        }else{
+            printf("opporator/whitespace not suported \n");
+            c = -1;
+        }
+
+        printf("%d",c);
+        printf("\n");
     }
-     printf("%d",c);
-     printf("\n");
-
-   
-
-
 }
