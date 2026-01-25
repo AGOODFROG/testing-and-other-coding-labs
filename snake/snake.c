5828c38bd8188b2cf2d9a96eeb7ca7f386679fd5
AGOODFROG <caden.j.quirion@gmail.com>
2026-01-25 13:14:40 -0500

    odd movment issue

--- snake/snake.c
+++ snake/snake.c
@@ -11,22 +11,22 @@ enum directions {
 };
 enum boardStates{
     space,
-    food,
-    snake
-}
+    snake,
+     food
+};
 
-int startGame(){
-    printf("TODO");
-}
+
 
 int draw(int board[10][10]){
     printf("\n");
     for(int i = 0; i < 10;i++ ){
-        for(int j = 0; j < 10; j++){printf("%d ", 0);}     
+        for(int j = 0; j < 10; j++){
+            printf("%d ", board[i][j]);
+        }     
         printf("\n");
     }
     printf("\n");
-    }
+ }
     
 
 
@@ -41,9 +41,17 @@ int main() {
     info.c_cc[VTIME] = 0;        
     tcsetattr(0, TCSANOW, &info);
     // game stuff
-    int snakePos[100][2] =  {1,1};
+    int snakePos[100][2] = {{1,1},{2,2},{3,3}};
     int board[10][10];
-    int prevSnakeSize = 1;
+    int snakeSize = sizeof(snakePos) / sizeof(snakePos[0]);
+    int foodPos[2] = {3,5};
+
+    for(int i = 0; i < 10;i++ ){
+        for(int j = 0; j < 10; j++){
+            board[i][j] = 0;
+        }     
+        
+    }
 
 
 
@@ -51,16 +59,49 @@ int main() {
     
 
     int a = 0;
-    while(1) {
-         if((keyInput = getchar())){
+    int startX;
+    int startY;
+    int x;
+    int y;
+    while(1)
+     {
+        startX = snakePos[0][0];
+        startY = snakePos[0][1];
+       
+        board[foodPos[0]][foodPos[1]] = food;
+        
+         if((keyInput = getchar()))
+         {
             //todo add sleep
-            while(a < 1000){
+            while(a < 1000)
+            {
                 a++;
             }
-            a=0;   
-            if(keyInput == up){printf("up");}
+            a=0; 
+            printf("\n");      
         }
+             for(int i = sizeof(snakePos)/(sizeof(snakePos[0])) - 1; i > 0; i--)
+            {
+                snakePos[i][0] = snakePos[i-1][0];
+                snakePos[i][1] = snakePos[i-1][1];
+            } 
+            
+            board[startX][startY+1] = snake;
+            snakePos[0][0] = startY+1;
+            snakePos[0][1] = startX;
+            for(int i = 0; i < 10; i++){
+                printf("%d ", snakePos[i][0]);
+                printf("%d, ", snakePos[i][1]);
+            }
+            
+            if(keyInput == up){printf("up");}
+            if(keyInput == down){printf("down");}
+            if(keyInput == right){printf("right");}
+            if(keyInput == left){printf("left");}
+    
+
         draw(board);
+        
         
     }
         //move the player
